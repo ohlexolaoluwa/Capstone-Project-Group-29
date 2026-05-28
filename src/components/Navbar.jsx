196cdb5ffe045c10a3c47c5c00385d9c6a08ae86
Ayo Akano <akanoayomipo@gmail.com>
2026-05-28 11:52:53 +0100

    Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        {/* Logo Section */}
+        <div className="navbar-logo">
+          <img src="/logo.png" alt="Planet Creative World Logo" />
+        </div>
+      </div>
+    </nav>
+  );
+}
