c60a3952cc18a82bf1feb0e1f00a7ea9dc69660d
Bejide Oluwatosin <oluwatossynbejide@gmail.com>
2026-06-02 04:20:59 +0100

    Fix import statements for TableSection and PlanetCard

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import './App.css';
 import Hero from './components/Hero/Hero';
 import VideoSection from './components/VideoSection/VideoSection';
-import TableSection from './components/TableSection/TableSection';
 import PlanetCard from './components/PlanetCard/PlanetCard';
 import TableSection from "./components/TableSection/TableSection";
-import PlanetCard from "./components/PlanetCard/PlanetCard";
 import FormSection from './components/FormSection/FormSection';
 import Footer from './components/Footer/Footer';
 
